79729634bcba7bd3ede628fbcf28c303b791dc70
Dennis Pagano <pagano@cqse.eu>
2017-07-23 17:27:29 +0200

    Improvements

--- src/main/java/model/CubeException.java
+++ src/main/java/model/CubeException.java
@@ -1,18 +1,25 @@
 package model;
 
+/**
+ * Exception for problems related to cubes. Currently based on
+ * {@link RuntimeException}, to be able to use within streams.
+ */
 public class CubeException extends RuntimeException {
 
 	/** Serial version id. */
 	private static final long serialVersionUID = 1L;
 
+	/** Constructor. */
 	public CubeException(String message) {
 		super(message);
 	}
 
+	/** Constructor. */
 	public CubeException(Throwable cause) {
 		super(cause);
 	}
 
+	/** Constructor. */
 	public CubeException(String message, Throwable cause) {
 		super(message, cause);
 	}
