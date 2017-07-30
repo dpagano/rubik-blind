1eeda514b284179f520f48fc677a8cb52b4c3665
Dennis Pagano <pagano@cqse.eu>
2017-07-30 13:17:15 +0200

    Comments

--- src/main/java/de/pagano/rubik/model/AbstractEdge.java
+++ src/main/java/de/pagano/rubik/model/AbstractEdge.java
@@ -5,22 +5,29 @@ import java.util.Objects;
 /** Generic base class for edges. */
 public abstract class AbstractEdge<T> {
 
+	/** The first value of the edge. */
 	private T firstValue;
+
+	/** The second value of the edge. */
 	private T secondValue;
 
+	/** Constructor. */
 	public AbstractEdge(T firstValue, T secondValue) {
 		this.firstValue = firstValue;
 		this.secondValue = secondValue;
 	}
 
+	/** @see #firstValue */
 	public T getFirstValue() {
 		return firstValue;
 	}
 
+	/** @see #secondValue */
 	public T getSecondValue() {
 		return secondValue;
 	}
 
+	/** Gets the inverse of this edge. */
 	public abstract AbstractEdge<T> getInvertedEdge();
 
 	@Override
