cbf82a15cb9dae944d79f7dfcf5b9f9c45117d5f
Dennis Pagano <pagano@cqse.eu>
2017-07-30 12:55:54 +0200

    WIP

--- src/main/java/de/pagano/rubik/model/colors/DefaultCubeColorSchema.java
+++ src/main/java/de/pagano/rubik/model/colors/DefaultCubeColorSchema.java
@@ -4,26 +4,27 @@ import org.apache.commons.collections4.bidimap.TreeBidiMap;
 
 import de.pagano.rubik.model.EFace;
 
+/** The default color schema of a 3x3 Rubik's cube. */
 public class DefaultCubeColorSchema implements ICubeColorSchema {
-	private static final TreeBidiMap<EFace, EColor> schema = new TreeBidiMap<>();
+	private static final TreeBidiMap<EFace, EColor> SCHEMA = new TreeBidiMap<>();
 
+	/** Constructor. */
 	public DefaultCubeColorSchema() {
-		schema.put(EFace.TOP, EColor.WHITE);
-		schema.put(EFace.FRONT, EColor.GREEN);
-		schema.put(EFace.LEFT, EColor.ORANGE);
-		schema.put(EFace.BACK, EColor.BLUE);
-		schema.put(EFace.RIGHT, EColor.RED);
-		schema.put(EFace.BOTTOM, EColor.YELLOW);
+		SCHEMA.put(EFace.TOP, EColor.WHITE);
+		SCHEMA.put(EFace.FRONT, EColor.GREEN);
+		SCHEMA.put(EFace.LEFT, EColor.ORANGE);
+		SCHEMA.put(EFace.BACK, EColor.BLUE);
+		SCHEMA.put(EFace.RIGHT, EColor.RED);
+		SCHEMA.put(EFace.BOTTOM, EColor.YELLOW);
 	}
 
 	@Override
 	public EColor getColor(EFace face) {
-		return schema.get(face);
+		return SCHEMA.get(face);
 	}
 
 	@Override
 	public EFace getFace(EColor color) {
-		return schema.getKey(color);
+		return SCHEMA.getKey(color);
 	}
-
 }
